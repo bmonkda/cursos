535139cdd19e429603e0a56b14052a5a2df6b08f
JOSE <bmonkdareg@gmail.com>
2022-04-28 14:52:05 -0400

    Mostrar listado de registros

---
+++ src/views/courses/CoursesList.vue
@@ -0,0 +1,40 @@
+<template>
+	<h1>Listado de cursos</h1>
+	<ul>
+		<li v-for="course in courses" :key="course.id">
+			<router-link :to="{name: 'CourseDetails', params: {id: course.id}}">
+				{{ course.title }}
+			</router-link>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			courses: []
+		}
+	},
+
+	created() {
+		this.getCourses();
+	},
+
+	methods: {
+		getCourses() {
+			this.axios.get('https://cursos-prueba.tk/api/courses')
+				.then(response => {
+					this.courses = response.data;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	},
+}
+</script>
+
+<style>
+
+</style>
