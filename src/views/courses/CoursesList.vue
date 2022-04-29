4defdfad499b32f24878cfa043bdbc8cb69a465a
JOSE <bmonkdareg@gmail.com>
2022-04-28 23:48:51 -0400

    Formulario de curso para guardar registro

--- src/views/courses/CoursesList.vue
+++ src/views/courses/CoursesList.vue
@@ -1,7 +1,33 @@
 <template>
 	<h1>Listado de cursos</h1>
+
+	<form>
+		<div>
+			<label for="title">Título</label>
+			<br>
+			<input id="title" type="text" placeholder="Ingrese el título del curso">
+		</div>
+		<br>
+		<div>
+			<label for="description">Descripción</label>
+			<br>
+			<textarea id="description" placeholder="Ingrese la descripción del curso"></textarea>
+		</div>
+		<br>
+		<div>
+			<!-- {{ categories }} -->
+			<label for="categoria">Categoría</label>
+			<br>
+			<select name="" id="categoria">
+				<option v-for="category in categories" :key=" 'category-' + category.id" :value="category.id">
+					{{ category.name }}
+				</option>
+			</select>
+		</div>
+	</form>
+
 	<ul>
-		<li v-for="course in courses" :key="course.id">
+		<li v-for="course in courses" :key="'course-' + course.id">
 			<router-link :to="{name: 'CourseDetails', params: {id: course.id}}">
 				{{ course.title }}
 			</router-link>
@@ -13,12 +39,14 @@
 export default {
 	data() {
 		return {
-			courses: []
+			courses: [],
+			categories: []
 		}
 	},
 
 	created() {
 		this.getCourses();
+		this.getCategories();
 	},
 
 	methods: {
@@ -26,6 +54,16 @@ export default {
 			this.axios.get('https://cursos-prueba.tk/api/courses')
 				.then(response => {
 					this.courses = response.data;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		},
+
+		getCategories() {
+			this.axios.get('https://cursos-prueba.tk/api/categories')
+				.then(response => {
+					this.categories = response.data;
 				})
 				.catch(error => {
 					console.log(error);
