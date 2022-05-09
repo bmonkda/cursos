7d2187c2d423362e9d276cf8cdfd30c82ba4846f
JOSE <bmonkdareg@gmail.com>
2022-05-09 08:38:21 -0400

    Guardar registro

--- src/views/courses/CoursesList.vue
+++ src/views/courses/CoursesList.vue
@@ -1,34 +1,37 @@
 <template>
 	<h1>Listado de cursos</h1>
 
-	<form>
+	<form @submit.prevent="saveCourse">
 		<div>
 			<label for="title">Título</label>
 			<br>
-			<input id="title" type="text" placeholder="Ingrese el título del curso">
+			<input v-model="course.title" id="title" type="text" placeholder="Ingrese el título del curso">
 		</div>
 		<br>
 		<div>
 			<label for="description">Descripción</label>
 			<br>
-			<textarea id="description" placeholder="Ingrese la descripción del curso"></textarea>
+			<textarea v-model="course.description" id="description" placeholder="Ingrese la descripción del curso"></textarea>
 		</div>
 		<br>
 		<div>
 			<!-- {{ categories }} -->
 			<label for="categoria">Categoría</label>
 			<br>
-			<select name="" id="categoria">
+			<select v-model="course.category_id" name="" id="categoria">
+				<option value="" selected disabled>Seleccione una categoría</option>
 				<option v-for="category in categories" :key=" 'category-' + category.id" :value="category.id">
 					{{ category.name }}
 				</option>
 			</select>
 		</div>
+		<br>
+		<button type="submit">Guardar</button>
 	</form>
 
 	<ul>
 		<li v-for="course in courses" :key="'course-' + course.id">
-			<router-link :to="{name: 'CourseDetails', params: {id: course.id}}">
+			<router-link :to="{ name: 'CourseDetails', params: { id: course.id } }">
 				{{ course.title }}
 			</router-link>
 		</li>
@@ -40,7 +43,12 @@ export default {
 	data() {
 		return {
 			courses: [],
-			categories: []
+			categories: [],
+			course: {
+				title: '',
+				description: '',
+				category_id: ''
+			}
 		}
 	},
 
@@ -64,6 +72,21 @@ export default {
 			this.axios.get('https://cursos-prueba.tk/api/categories')
 				.then(response => {
 					this.categories = response.data;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		},
+
+		saveCourse() {
+			this.axios.post('https://cursos-prueba.tk/api/courses', this.course)
+				.then(response => {
+					this.courses.push(response.data);
+					this.course = {
+						title: '',
+						description: '',
+						category_id: ''
+					}
 				})
 				.catch(error => {
 					console.log(error);
